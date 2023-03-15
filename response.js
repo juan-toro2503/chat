5deca63633958039615783ba2603a0ebce5b9de5
juan-toro <jftoro940@misena.edu.co>
2023-03-15 01:01:45 -0500

    creando las rutas y controladores

--- response.js
+++
@@ -1,8 +0,0 @@
-exports.success=function(req,res,message){
-    res.send({error:'',
-body:message})
-}
-
-exports.error=function(req,res,message,status){
-    res.send({error:message,codigo:status,})
-}
